b5340d06891cd2c71028fd9bb6af4850c1a40eb2
serraguti <pacoserranox@gmail.com>
2026-03-18 11:36:48 +0100

    Add project files.

---
+++ MvcCoreTestingNuget/Controllers/JugadoresController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JugadoresNugetPGS.Repositories;
+using JugadoresNugetPGS.Models;
+
+namespace MvcCoreTestingNuget.Controllers
+{
+    public class JugadoresController : Controller
+    {
+        private RepositoryJugadores repo;
+
+        public JugadoresController(RepositoryJugadores repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            List<Jugador> players = this.repo.GetJugadores();
+            return View(players);
+        }
+    }
+}
